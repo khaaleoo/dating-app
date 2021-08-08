73a0d0444570a271fdd5360c064322de7c640efc
khaleo <khalx@nhanlucsieuviet.com>
2021-08-08 14:02:05 +0700

    init

--- App.js
+++ App.js
@@ -1,12 +1,70 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState, useEffect, useRef } from 'react';
+import { StyleSheet, Text, View, Alert } from 'react-native';
+import Constants from "expo-constants";
+import TopBar from "./components/TopBar";
+import axios from 'axios';
+import SwipeableImage from './components/SwipeableImage';
+import BottomBar from './components/BottomBar';
+import Swipes from './components/Swipes';
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const swipesRef = useRef(null)
+
+  async function fetchUsers() {
+    try {
+      const { data } = await axios.get('https://randomuser.me/api/?gender=female&results=50');
+      setUsers(data.results)
+      console.log(data.results)
+    } catch (err) {
+      console.log(err)
+      Alert.alert('Error getting users', '', [{ text: 'Retry', onPress: () => fetchUsers() }])
+    }
+  }
+
+  useEffect(() => {
+    fetchUsers()
+  }, [])
+
+  function handleLike() {
+    console.log('like')
+    nextUser()
+  }
+
+  function handlePass() {
+    console.log('pass')
+    nextUser()
+  }
+
+  function nextUser() {
+    const nextIndex = users.length - 2 === currentIndex ? 0 : currentIndex + 1
+    setCurrentIndex(nextIndex)
+  }
+
+  function handleLikePress() {
+    swipesRef.current.openLeft()
+  }
+  function handlePassPress() {
+    swipesRef.current.openRight()
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TopBar />
+      <View style={styles.swipes}>
+        {users.length > 1 && users.map((u, i) => (
+          currentIndex === i &&
+          <Swipes
+            key={i}
+            ref={swipesRef}
+            currentIndex={currentIndex}
+            users={users}
+            handleLike={handleLike}
+            handlePass={handlePass}
+          />
+        ))}
+      </View>
+      <BottomBar handleLikePress={handleLikePress} handlePassPress={handlePassPress} />
     </View>
   );
 }
@@ -14,8 +72,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight
   },
+  swipes: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 2.65,
+    elevation: 7
+  }
 });
